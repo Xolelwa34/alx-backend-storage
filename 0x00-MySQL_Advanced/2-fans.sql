98f1502abf31dcc6f9a2e88846e0ae6c0fa00094
Xolelwa34 <ciliaswekile@gmail.com>
2024-10-17 09:49:45 -0700

    ource table containing information about bands

--- 0x00-MySQL_Advanced/2-fans.sql
+++ 0x00-MySQL_Advanced/2-fans.sql
@@ -1,5 +1,9 @@
-SELECT origin, SUM(fans) AS nb_fans
-FROM metal_bands
-GROUP BY origin
-ORDER BY nb_fans DESC;
-
+SELECT
+  origin,            -- Selects the country of origin of each band
+  SUM(fans) nb_fans  -- Sums the number of fans for all bands from the same country
+FROM
+  metal_bands        -- Source table containing information about bands and their fans
+GROUP BY
+  origin             -- Groups the results by the country of origin
+ORDER BY
+  nb_fans DESC       -- Orders the results in descending order of total fans
