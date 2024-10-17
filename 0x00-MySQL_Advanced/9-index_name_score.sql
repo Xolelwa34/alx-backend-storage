ff7857b27f27b2a42c19a0fec6fe55500db7c1e4
Xolelwa34 <ciliaswekile@gmail.com>
2024-10-17 10:46:35 -0700

    adding index name

--- 0x00-MySQL_Advanced/9-index_name_score.sql
+++ 0x00-MySQL_Advanced/9-index_name_score.sql
@@ -1,3 +1,4 @@
 -- Create index if it does not exist
 
-CREATE INDEX IF NOT EXISTS idx_name_first_score ON names (LEFT(name, 1), score);
+CREATE INDEX idx_name_first_score ON names (name(1), score);
+
