a8d5d08d9fb4ac87644a1221e0ab646f0e0582ea
Xolelwa34 <ciliaswekile@gmail.com>
2024-10-17 10:37:14 -0700

    adding delimeters

---
+++ 0x00-MySQL_Advanced/5-valid_email.sql
@@ -0,0 +1,11 @@
+-- Create trigger if it does not exist
+
+DELIMITER //
+CREATE TRIGGER reset_valid_email BEFORE UPDATE ON users
+FOR EACH ROW
+BEGIN
+    IF OLD.email != NEW.email THEN
+        SET NEW.valid_email = 0;
+    END IF;
+END;
+DELIMITER ;
